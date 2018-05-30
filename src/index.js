8667b11b8170588fc4e8ac77f02b766dba748d95
hbulgarini <hbulgarini@gmail.com>
2018-05-30 20:04:37 -0300

    commit inicial

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from 'views/main/App';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+import reducer_reminders from 'reducers/reducer_reminder';
+
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducer_reminders, composeEnhancers(
+    applyMiddleware(thunk)
+  ));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+	 document.getElementById('app'))
